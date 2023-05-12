1598b9a4c5725eab8b96fa1490ea53f34988680f
Joaquin Brunengo <jbrunengo@gmail.com>
2023-05-12 12:13:46 -0300

    Problema con Total

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,34 +1,42 @@
-import {createContext, useState} from 'react'
+import { createContext, useState } from 'react'
 
-export const CartContext = createContext ({
+export const CartContext = createContext({
     cart: []
 })
 
-export const CartProvider = ({children}) =>{
-    const [cart, setCart] = useState ([])
+export const CartProvider = ({ children }) => {
+    const [cart, setCart] = useState([])
+    const [totalQuantity, setTotalQuantity] = useState(0)
+    const [total, setTotal] = useState(0)
 
-    const addItem =(item, quantity) => {
-        if(!isInCart (item.id)) {
-            setCart (prev => [...prev, {...item, quantity}])
-        } else {
-            console.error ('Producto ya agregado')
+    const addItem = (item, quantity) => {
+        if (!isInCart(item.id)) {
+                setCart(prev => [...prev, { ...item, quantity }])
+                setTotalQuantity(prev => prev + quantity)
+                setTotal(prev => prev + item.price * quantity)
+            } else {
+                console.error ('Producto ya agregado')
         }
     }
-    const removeItem = (itemId) =>{
-        const cartUpdated = cart.filter (prod => prod.id !== itemId)
-        setCart(cartUpdated)
+    const removeItem = (itemId) => {
+        const cartUpdated = cart.find(prod => prod.id !== itemId)
+        setCart(prev => prev.filter(prod => prod.id !== itemId))
+        setTotalQuantity(prev => prev - cartUpdated.quantity)
+        setTotal(prev => prev - cartUpdated.price * cartUpdated.quantity)
     }
 
     const clearCart = () => {
-        setCart ([])
+        setCart([])
+        setTotalQuantity(0)
+        setTotal(0)
     }
 
     const isInCart = (itemId) => {
-        return cart.some(prod => prod.id ===itemId)
+        return cart.some(prod => prod.id === itemId)
     }
 
     return (
-        <CartContext.Provider value={{cart, addItem, removeItem, clearCart}}>
+        <CartContext.Provider value={{ cart, addItem, removeItem, clearCart, totalQuantity, total }}>
             {children}
         </CartContext.Provider>
     )
